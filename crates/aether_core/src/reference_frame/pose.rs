ecf86b6882e9c6aca38546038e60cbdb4cfbe85d
Michael Angeles <michael.g.angeles21@gmail.com>
2025-10-25 14:02:07 -0500

    minor cleanup

--- crates/aether_core/src/reference_frame/pose.rs
+++ crates/aether_core/src/reference_frame/pose.rs
@@ -6,7 +6,7 @@ use num_traits::Float;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Pose<T: Float, From: ReferenceFrame> {
-    /// DCM from the solid's Local frame to the Assembly frame (Local → Assembly)
+    /// DCM from the solid's Local frame to the Assembly frame (Local -> Assembly)
     pub R_local_to_assembly: DirectionCosineMatrix<T, From, Assembly<T>>,
     /// Center-of-mass position in the Assembly frame
     pub r_cm_in_assembly: Cartesian<T, Assembly<T>>,
