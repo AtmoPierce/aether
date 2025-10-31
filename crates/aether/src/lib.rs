4446766dcb38569f534af10d0cbfc8e183e22972
Michael Angeles <michael.g.angeles21@gmail.com>
2025-10-30 22:35:16 -0500

    Expose all crates to aether.

--- crates/aether/src/lib.rs
+++ crates/aether/src/lib.rs
@@ -1,4 +1,12 @@
-pub use aether_core::{
-    attitude, coordinate, math, models, numerical_methods, reference_frame, utils,
-};
+pub use aether_core::*;
+pub use aether_benchmark::*;
+pub use aether_fluids::*;
+pub use aether_graphics::*;
+pub use aether_ml::*;
+pub use aether_models::*;
+pub use aether_opt::*;
+pub use aether_rand::*;
+pub use aether_shapes::*;
 
+#[cfg(feature = "std")]
+pub use aether_stats::*;
