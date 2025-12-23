196d31ac86a863fe312000c17cdd5ef472005745
Michael Angeles <Michael.G.Angeles21@gmail.com>
2025-12-23 15:04:39 -0600

    Real fixes...

--- crates/aether_core/src/utils/mod.rs
+++ crates/aether_core/src/utils/mod.rs
@@ -1,12 +1,12 @@
-// REPL
-#[cfg(feature = "repl")]
-pub mod parse;
-#[cfg(feature = "repl")]
-pub use parse::*;
-#[cfg(feature = "repl")]
-pub mod repl;
-#[cfg(feature = "repl")]
-pub use repl::*;
+// REPL -- needs to move out of core
+// #[cfg(feature = "repl")]
+// pub mod parse;
+// #[cfg(feature = "repl")]
+// pub use parse::*;
+// #[cfg(feature = "repl")]
+// pub mod repl;
+// #[cfg(feature = "repl")]
+// pub use repl::*;
 
 // Unit Conversions
 pub mod angle_conversion;
