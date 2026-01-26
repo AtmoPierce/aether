242b2ab4d793640e972d672745a248d33dfbf32c
supergoodname77 <epascuales@outlook.com>
2026-01-25 19:08:50 -0600

    Changes for bincode on quaternions, vectors, and cartesian as well as unit tests

--- crates/aether_test/src/assert/mod.rs
+++ crates/aether_test/src/assert/mod.rs
@@ -1,6 +1,7 @@
 pub mod vector;
 pub mod matrix;
 pub mod function;
+pub mod coding;
 
 #[cfg(test)]
 #[path = "tests/mod.rs"]
--- crates/aether_test/src/assert/tests/mod.rs
+++ crates/aether_test/src/assert/tests/mod.rs
@@ -1,5 +1,6 @@
 pub mod vector;
 pub mod matrix;
+pub mod coding;
 
 #[cfg(feature = "plots")]
 pub mod plots;
